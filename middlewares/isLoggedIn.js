849dbc3c2c958a24f4271a1147fb1c97f98c1fbe
Michle99 <sammiyonyx@gmail.com>
2023-11-13 01:27:45 -0500

    Added isLoggedIn middleware to middleware folder

---
+++ middlewares/isLoggedIn.js
@@ -0,0 +1,10 @@
+// Middleware to check if the user is logged in
+const isLoggedIn = (req, res, next) => {
+    if (req.session && req.session.user) {
+      return next(); 
+    } else {
+      res.redirect('/login');
+    }
+};
+
+module.exports = isLoggedIn;
