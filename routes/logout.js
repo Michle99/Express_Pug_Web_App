02ecbf491f6e0043fc0aab973698d08e10cbaef9
Michle99 <sammiyonyx@gmail.com>
2023-11-13 01:34:03 -0500

    Added logout routes

---
+++ routes/logout.js
@@ -0,0 +1,15 @@
+// routes/logout.js
+const express = require('express');
+const router = express.Router();
+
+// GET - Handle user logout
+router.get('/', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error('Error destroying session:', err);
+    }
+    res.redirect('/login');
+  });
+});
+
+module.exports = router;
