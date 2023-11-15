022bafa671e39f36247ec8eae6c3b529ab775ae7
Michle99 <sammiyonyx@gmail.com>
2023-11-14 21:15:12 -0500

    Add initiliazeDate function

--- routes/add.js
+++ routes/add.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
-const { getImages, addImage, getImagesData } = require('../data/image');
+const { initializeData, addImage, getImagesData } = require('../data/image');
 const isLoggedIn = require('../middlewares/isLoggedIn');
 
 // Set up multer storage configuration for single image upload
